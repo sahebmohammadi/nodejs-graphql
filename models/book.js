225b865906aaebcf32583ec8ef42b48482c00ed7
saheb mohammadi <saheb.put92@gmail.com>
2022-11-23 00:26:41 +0330

    add mongoose model

---
+++ models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const BookSchema = new Schema({
+  title: { type: String, requored: true },
+  genere: { type: String, required: true },
+  authorId: {
+    type: ObjectId,
+    ref: "Author",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Book", BookSchema);
